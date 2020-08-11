a30b54491c9cceec751e0f350afe9de4b998b303
Bahindiemma <bahindiemmanuel20@gmail.com>
2020-08-11 12:09:07 +0300

    coded the index.html

---
+++ reverse_num.js
@@ -0,0 +1,12 @@
+function beautifulDays(i, j, k) {
+
+    let finalAr = [];
+    let num;
+    let bDays = 0;
+    for (let a = i; a <= j; a++) {
+        finalAr = a.push();
+
+    }
+    return num;
+}
+console.log(beautifulDays(20, 23, 6))
